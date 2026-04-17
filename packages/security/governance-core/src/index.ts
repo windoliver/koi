22a5d45b16c23e3ccea4755b7f7ab528cf399153
Tao Feng <taofeng.nju@gmail.com>
2026-04-16 20:05:22 -0700

    feat(governance-core): public API surface + snapshot (#1392)

--- packages/security/governance-core/src/index.ts
+++ packages/security/governance-core/src/index.ts
@@ -1,1 +1,25 @@
-export {};
+export type {
+  AlertCallback,
+  AlertTracker,
+  AlertTrackerConfig,
+} from "./alert-tracker.js";
+export { createAlertTracker } from "./alert-tracker.js";
+
+export type {
+  GovernanceMiddlewareConfig,
+  UsageCallback,
+  ViolationCallback,
+} from "./config.js";
+export { DEFAULT_ALERT_THRESHOLDS, validateGovernanceConfig } from "./config.js";
+
+export type { CostCalculator, PricingEntry } from "./cost-calculator.js";
+export { createFlatRateCostCalculator } from "./cost-calculator.js";
+
+export {
+  createGovernanceMiddleware,
+  GOVERNANCE_MIDDLEWARE_NAME,
+  GOVERNANCE_MIDDLEWARE_PRIORITY,
+} from "./governance-middleware.js";
+
+export type { NormalizedUsage } from "./normalize-usage.js";
+export { normalizeUsage } from "./normalize-usage.js";
