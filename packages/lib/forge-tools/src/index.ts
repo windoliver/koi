5a5ac532c8deee30cf48da9aef2cc709c7a1445a
SophiaWJ <jingwangsophia@gmail.com>
2026-04-26 23:03:39 -0700

    feat(forge-tools): wire index re-exports

--- packages/lib/forge-tools/src/index.ts
+++ packages/lib/forge-tools/src/index.ts
@@ -1,2 +1,5 @@
-// Re-exports added by subsequent tasks.
-export {};
+export { createInMemoryForgeStore } from "./memory-store.js";
+export { createForgeInspectTool, type ForgeInspectDeps } from "./tools/forge-inspect.js";
+export { createForgeListTool, type ForgeListDeps } from "./tools/forge-list.js";
+export { createForgeMiddlewareTool, type ForgeMiddlewareDeps } from "./tools/forge-middleware.js";
+export { createForgeToolTool, type ForgeToolDeps } from "./tools/forge-tool.js";
